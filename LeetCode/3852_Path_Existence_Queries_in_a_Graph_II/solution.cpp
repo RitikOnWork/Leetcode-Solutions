34cc8a24c177b0b0e32355564324556637c6a124
Ritik Raj <ritikraj2186@gmail.com>
2026-07-10 11:08:02 +0530

    Solves 3852 - Path Existence Queries in a Graph II (C++)

---
+++ LeetCode/3852_Path_Existence_Queries_in_a_Graph_II/solution.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<pair<int,int>> a;
+        a.reserve(n);
+        for (int i = 0; i < n; i++) a.push_back({nums[i], i});
+        sort(a.begin(), a.end());
+
+        vector<int> pos(n);
+        for (int i = 0; i < n; i++) pos[a[i].second] = i;
+
+        vector<int> nxt(n);
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            while (r + 1 < n && a[r + 1].first - a[i].first <= maxDiff) r++;
+            nxt[i] = r;
+        }
+
+        vector<int> comp(n);
+        int id = 0;
+        comp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (a[i].first - a[i - 1].first > maxDiff) id++;
+            comp[i] = id;
+        }
+
+        const int LOG = 18; // 2^17 > 1e5
+        vector<vector<int>> up(LOG, vector<int>(n));
+        up[0] = nxt;
+        for (int k = 1; k < LOG; k++) {
+            for (int i = 0; i < n; i++) {
+                up[k][i] = up[k - 1][ up[k - 1][i] ];
+            }
+        }
+
+        vector<int> ans;
+        ans.reserve(queries.size());
+
+        for (auto &q : queries) {
+            int u = pos[q[0]];
+            int v = pos[q[1]];
+            if (u == v) {
+                ans.push_back(0);
+                continue;
+            }
+            if (u > v) swap(u, v);
+
+            if (comp[u] != comp[v]) {
+                ans.push_back(-1);
+                continue;
+            }
+
+            int cur = u;
+            int steps = 0;
+            for (int k = LOG - 1; k >= 0; k--) {
+                if (up[k][cur] < v) {
+                    cur = up[k][cur];
+                    steps += 1 << k;
+                }
+            }
+            ans.push_back(steps + 1);
+        }
+
+        return ans;
+    }
+};
