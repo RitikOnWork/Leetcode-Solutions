6d02b3a833c42f3d75dffe4676b93afe9e850da6
Ritik Raj <ritikraj2186@gmail.com>
2026-08-30 12:37:41 +0530

    Solves 2212 - Removing Minimum and Maximum From Array (C++)

---
+++ LeetCode/2212_Removing_Minimum_and_Maximum_From_Array/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n = nums.size();
+        int minIdx = 0, maxIdx = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < nums[minIdx])
+                minIdx = i;
+            if (nums[i] > nums[maxIdx])
+                maxIdx = i;
+        }
+
+        int left = min(minIdx, maxIdx);
+        int right = max(minIdx, maxIdx);
+
+        int removeFront = right + 1;
+        int removeBack = n - left;
+        int removeBoth = (left + 1) + (n - right);
+
+        return min({removeFront, removeBack, removeBoth});
+    }
+};
